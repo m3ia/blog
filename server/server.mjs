a1800d20372fa7e23847147366ba7601e6eebcbe
m3ia <meianatividad@gmail.com>
2022-10-11 21:51:51 -0700

    Add all GETs

--- server/server.mjs
+++ server/server.mjs
@@ -15,3 +15,33 @@ app.listen(PORT, () => {
 })
 app.get('/', (req, res) => res.json('Welcome to the API'));
 
+// GET - All Posts --------------------------------------------------------
+app.get('/posts', async function (req, res) {
+  try {
+    const posts = await db.any('SELECT * FROM posts');
+    res.send(posts);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
+// GET - All Users --------------------------------------------------------
+app.get('/users', async function (req, res) {
+  try {
+    const users = await db.any('SELECT * FROM users');
+    res.send(users);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
+// GET - All Contacts --------------------------------------------------------
+app.get('/contacts', async function (req, res) {
+  try {
+    const contacts = await db.any('SELECT * FROM contacts');
+    res.send(contacts);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
