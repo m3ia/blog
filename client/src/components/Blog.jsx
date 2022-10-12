9d6b5b80e11f850fe25131025b77123f050189e4
m3ia <meianatividad@gmail.com>
2022-10-11 23:13:32 -0700

    Render posts

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,11 +1,42 @@
 import NavBar from "./NavBar";
 import Post from "./Post";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const Blog = () => {
-  const views = ["blog", "about", "contact", "newPost"];
   const [view, setView] = useState("blog");
+  const views = ["blog", "about", "contact", "newPost"];
+  const [posts, setPosts] = useState([]);
 
+  // Get Posts
+  useEffect(() => {
+    const getPosts = async () => {
+      await fetch("http://localhost:8080/posts")
+        .then((res) => res.json())
+        .then((res) => {
+          console.log("res", res);
+          let postsArr = [];
+          for (let item of res) {
+            let postObject = {
+              title: item.title,
+              photo: item.photo,
+              content: item.content,
+              likes: item.subheading,
+              subheading: item.subheading,
+              published: item.published,
+              private: item.private,
+              datePosted: item.date_posted,
+              dateEdited: item.date_edited,
+              dateCreated: item.date_created,
+            };
+
+            postsArr.push(postObject);
+          }
+
+          setPosts(postsArr);
+        });
+    };
+    getPosts();
+  }, []);
   return (
     <>
       <NavBar setView={setView} />
@@ -17,8 +48,9 @@ const Blog = () => {
               <h2>THIS IS THE SUBHEADER & HELLOO TO THE SECOND LINE</h2>
             </div>
             <div className="posts-container">
-              <Post />
-              <Post />
+              {posts.map((post, ind) => {
+                return <Post key={ind} post={post} ind={ind} />;
+              })}
             </div>
           </>
         )}
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,31 +1,14 @@
-const Post = () => {
+const Post = ({post, ind}) => {
   return (
     <div className="post-div">
-      <div className="post-date">Post Date here</div>
+      <div className="post-date">{post.datePosted}</div>
       <div className="post-body">
         <div className="post-header">
-          <h3>Post Header Here</h3>
-          <h4>Post Subheader here</h4>
+          <h3>{post.title}</h3>
+          <h4>{post.subheading}</h4>
         </div>
         <div className="post-content">
-          <p>Post Content here</p>
-          <p>
-            "But I must explain to you how all this mistaken idea of denouncing
-            pleasure and praising pain was born and I will give you a complete
-            account of the system, and expound the actual teachings of the great
-            explorer of the truth, the master-builder of human happiness. No one
-            rejects, dislikes, or avoids pleasure itself, because it is
-            pleasure, but because those who do not know how to pursue pleasure
-            rationally encounter consequences that are extremely painful. Nor
-            again is there anyone who loves or pursues or desires to obtain pain
-            of itself, because it is pain, but because occasionally
-            circumstances occur in which toil and pain can procure him some
-            great pleasure. To take a trivial example, which of us ever
-            undertakes laborious physical exercise, except to obtain some
-            advantage from it? But who has any right to find fault with a man
-            who chooses to enjoy a pleasure that has no annoying consequences,
-            or one who avoids a pain that produces no resultant pleasure?"
-          </p>
+          <p>{post.content}</p>
         </div>
       </div>
     </div>
