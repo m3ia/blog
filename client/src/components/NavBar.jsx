4bf787b92aa0417b52710da9c0abe2dcb66bc1d7
m3ia <meianatividad@gmail.com>
2022-10-13 13:50:41 -0700

    Resize text for NavBar btns

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,16 +2,16 @@ const NavBar = ({setView}) => {
   return (
     <div className="navbar-container">
       <div className="navbar-btn" onClick={() => setView("blog")}>
-        <h2>Blog</h2>
+        <h4>Blog</h4>
       </div>
       <div className="navbar-btn" onClick={() => setView("about")}>
-        <h2>About</h2>
+        <h4>About</h4>
       </div>
       <div className="navbar-btn" onClick={() => setView("contact")}>
-        <h2>Contact</h2>
+        <h4>Contact</h4>
       </div>
       <div className="navbar-btn" onClick={() => setView("newPost")}>
-        <h2>New Post</h2>
+        <h4>New Post</h4>
       </div>
     </div>
   );
